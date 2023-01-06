76f6260fad10d0b74d2a0868a324a35e00aec987
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-01-06 18:26:09 +0500

    Git Init

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Learning Next.JS!</title>
+        <title>LEARNING NEXT.JS 12!</title>
       </Head>
 
       <h1>THIS IS HOME PAGE</h1>
